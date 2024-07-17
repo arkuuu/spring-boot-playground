8eff3996c27d419a0e2358820680742394c03769
arkuuu <arkuuu@users.noreply.github.com>
2024-07-17 23:19:32 +0200

    Return a json record instead of plain text

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -4,11 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Greeting;
+
 @RestController
 public class GreetingController {
 
+    private long counter = 0;
+
     @GetMapping(path = "/greeting")
-    public String greeting(@RequestParam(defaultValue = "User") String name) {
-        return "Hello " + name;
+    public Greeting greeting(@RequestParam(defaultValue = "User") String name) {
+        return new Greeting(
+                counter++,
+                String.format("Hello, %s", name));
     }
 }
