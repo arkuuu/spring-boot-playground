6b1c1c796e91166043773542a88aec4019ef24e7
arkuuu <arkuuu@users.noreply.github.com>
2024-07-17 23:22:05 +0200

    Use `AtomicLong` instead of long
    This seems to be important for concurrency and threads. See https://stackoverflow.com/a/35547532/4946451

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,12 @@ import com.example.demo.model.Greeting;
 @RestController
 public class GreetingController {
 
-    private long counter = 0;
+    private AtomicLong counter = new AtomicLong();
 
     @GetMapping(path = "/greeting")
     public Greeting greeting(@RequestParam(defaultValue = "User") String name) {
         return new Greeting(
-                counter++,
+                counter.getAndIncrement(),
                 String.format("Hello, %s", name));
     }
 }
